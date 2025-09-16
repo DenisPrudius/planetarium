eb8850f1de6360601a6463c7aed8c980ae94546e
benfict <benfict@gmail.com>
2025-09-16 21:51:52 +0300

    add Astronomy show $ ShowTheme models to show app

--- show/models.py
+++ show/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class AstronomyShow(models.Model):
+    id = models.IntegerField(primary_key=True)
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    themes = models.ManyToManyField("ShowTheme", related_name="astronomy_shows")
+
+    def __str__(self):
+        return self.title
+
+
+class ShowTheme(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+
