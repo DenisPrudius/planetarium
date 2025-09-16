82646c3a9b96704805d4dbf0fcdef9d1d8a545af
benfict <benfict@gmail.com>
2025-09-17 00:20:11 +0300

    add AstronomyDetailSerializer

--- show/serializers.py
+++ show/serializers.py
@@ -2,15 +2,23 @@ from rest_framework import serializers
 from show.models import AstronomyShow, ShowTheme
 
 
-class AstronomyShowSerializer(serializers.ModelSerializer):
-    themes = AstronomyShowSerializer(many=True, read_only=True)
+class ShowThemeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ShowTheme
+        fields = ["id", "name"]
+
+class AstronomyShowListSerializer(serializers.ModelSerializer):
+    themes = ShowThemeSerializer(many=True, read_only=True)
 
     class Meta:
         model = AstronomyShow
         fields = ["id", "title", "themes"]
 
 
-class ShowThemeSerializer(serializers.ModelSerializer):
+class AstronomyShowDetailSerializer(serializers.ModelSerializer):
+    themes = ShowThemeSerializer(many=True, read_only=True)
+
     class Meta:
-        model = ShowTheme
-        fields = ["id", "name"]
+        model = AstronomyShow
+        fields = ["id", "title", "description", "themes"]
+
