caeac5a2e83c8802e2b1e6f57d997d65bbad159c
benfict <benfict@gmail.com>
2025-09-18 00:55:36 +0300

    add at ticket app  in views.py ReservationViewSet

--- ticket/views.py
+++ ticket/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+from planetarium.mixins import ActionSerializerPermissionMixin
+from ticket.models import Reservation
+from ticket.serializers import ReservationSerializer
+
+
+class ReservationViewSet(ActionSerializerPermissionMixin, viewsets.ModelViewSet):
+    queryset = Reservation.objects.all()
+    serializer_class = ReservationSerializer
+    permission_classes = [IsAuthenticated]
+
+    action_serializer_classes = {
+        "list": ReservationSerializer,
+        "retrieve": ReservationSerializer,
+        "create": ReservationSerializer,
+    }
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
