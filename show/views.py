1a9228c444915bc1aa9a81ff9da3540464ceab4f
benfict <benfict@gmail.com>
2025-09-18 00:40:49 +0300

    add permision mixin to viewset

--- show/views.py
+++ show/views.py
@@ -2,12 +2,13 @@ from django.shortcuts import render
 from rest_framework import viewsets, permissions
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
 
+from planetarium.mixins import ActionSerializerPermissionMixin
 from .serializers import AstronomyShowListSerializer, AstronomyShowDetailSerializer
 
 from show.models import AstronomyShow
 
 
-class AstronomyShowViewSet(viewsets.ModelViewSet):
+class AstronomyShowViewSet(ActionSerializerPermissionMixin, viewsets.ModelViewSet):
     queryset = AstronomyShow.objects.all()
     serializer_class = AstronomyShowDetailSerializer
     permission_classes = [IsAuthenticated]
