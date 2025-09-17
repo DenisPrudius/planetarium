9f5d28acff52ddfdbc3aab93057116d3f18577cf
benfict <benfict@gmail.com>
2025-09-17 23:43:34 +0300

    add AstronomyShowViewSet to views.py

--- show_sessions/views.py
+++ show_sessions/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from rest_framework.response import Response
 
-# Create your views here.
+from show_sessions.models import ShowSession
+from show_sessions.serializers import ShowSessionListSerializer, ShowSessionCreateSerializer
+
+
+class ShowSessionViewSet(viewsets.ModelViewSet):
+    queryset = ShowSession.objects.select_related("astronomy_show", "planetarium_dome").all()
+    serializer_class = ShowSessionListSerializer
+    permission_classes = [IsAuthenticated]
+
+    action_serializer_classes = {
+        "list": ShowSessionListSerializer,
+        "retrieve": ShowSessionListSerializer,
+        "create": ShowSessionCreateSerializer,
+    }
+
+    action_permission_classes = {
+        "destroy": [IsAdminUser],
+    }
+
+    @action(detail=True, methods=["get"], url_path="available-seats")
+    def available_seats(self, request, pk=None):
+        session = self.get_object()
+        dome = session.planetarium_dome
+        total = dome.rows * dome.seats_in_row
+        booked = session.tickets.count()
+        return Response({"total_seats": total, "booked": booked, "available": total - booked})
