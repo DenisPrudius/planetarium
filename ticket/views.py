921df6bd33d789f6dc91fc97bd88d463edd816a8
benfict <benfict@gmail.com>
2025-09-21 20:27:43 +0300

    add TicketViewSet to views.py in ticket app and add get_queryset func to ReservationViewSet

--- ticket/views.py
+++ ticket/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
 from rest_framework import viewsets
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, IsAdminUser, AllowAny
 
 from planetarium.mixins import ActionSerializerPermissionMixin
-from ticket.models import Reservation
-from ticket.serializers import ReservationSerializer
+from ticket.models import Reservation, Ticket
+from ticket.serializers import ReservationSerializer, TicketSerializer, ReservationCreateSerializer, \
+    ReservationDetailSerializer
 
 
 class ReservationViewSet(ActionSerializerPermissionMixin, viewsets.ModelViewSet):
@@ -14,9 +15,33 @@ class ReservationViewSet(ActionSerializerPermissionMixin, viewsets.ModelViewSet)
 
     action_serializer_classes = {
         "list": ReservationSerializer,
-        "retrieve": ReservationSerializer,
-        "create": ReservationSerializer,
+        "retrieve": ReservationDetailSerializer,
+        "create": ReservationCreateSerializer,
+        "update": ReservationDetailSerializer,
+        "partial_update": ReservationCreateSerializer,
     }
+
+    action_permission_classes = {
+        "destroy": [IsAdminUser],
+    }
+
+    def get_queryset(self):
+        return self.queryset.filter(user=self.request.user)
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
+
+class TicketViewSet(ActionSerializerPermissionMixin, viewsets.ModelViewSet):
+    queryset = Ticket.objects.select_related("show_session", "reservation").all()
+    serializer_class = TicketSerializer
+    permission_classes = [IsAuthenticated]
+
+    action_serializer_classes = {
+        "list": TicketSerializer,
+        "retrieve": TicketSerializer,
+        "create": TicketSerializer,
+    }
+
+    action_permission_classes = {
+        "destroy": [IsAdminUser],
+    }
