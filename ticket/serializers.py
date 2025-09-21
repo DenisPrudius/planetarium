fffb4900858b688b457a1d2c17d781215bb7c202
benfict <benfict@gmail.com>
2025-09-21 20:23:27 +0300

    Recode and add "create" func to ReservationCreate serializer and add ReservationDetail

--- ticket/serializers.py
+++ ticket/serializers.py
@@ -1,17 +1,42 @@
+from django.contrib.auth import get_user_model
+from django.contrib.auth.models import AbstractUser
+from django.db import transaction
 from rest_framework import serializers
-from .models import Ticket, Reservation
 
+from show_sessions.serializers import ShowSessionListSerializer
+from .models import Ticket, Reservation
 
 class TicketSerializer(serializers.ModelSerializer):
     class Meta:
         model = Ticket
-        fields = ["id", "row", "seat", "show_session", "reservation"]
+        fields = ["id", "row", "seat", "show_session"]
+
 
 
 class ReservationSerializer(serializers.ModelSerializer):
-    tickets = TicketSerializer(many=True, read_only=True, source="tickets")
+    tickets = TicketSerializer(many=True, read_only=False, allow_empty=False)
 
     class Meta:
         model = Reservation
         fields = ["id", "user", "created_at", "tickets"]
         read_only_fields = ["user", "created_at"]
+
+class ReservationCreateSerializer(serializers.ModelSerializer):
+    tickets = TicketSerializer(many=True, read_only=False, allow_empty=False)
+    class Meta:
+        model = Reservation
+        fields = ["id", "created_at", "tickets"]
+
+    def create(self, validated_data):
+        with transaction.atomic():
+            tickets_data = validated_data.pop("tickets")
+            reservation = Reservation.objects.create(**validated_data)
+            for ticket_data in tickets_data:
+                Ticket.objects.create(reservation=reservation, **ticket_data)
+            return reservation
+
+class ReservationDetailSerializer(serializers.ModelSerializer):
+    tickets = TicketSerializer(many=True, read_only=False, allow_empty=False)
+    class Meta:
+        model = Reservation
+        fields = ["id", "user", "created_at", "tickets"]
