8fbfe1b6d90366c1d57f8a8f661c0e1f889dc831
benfict <benfict@gmail.com>
2025-09-21 20:24:43 +0300

    Fix urls

---
+++ ticket/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import ReservationViewSet, TicketViewSet
+
+app_name = "ticket"
+
+router = DefaultRouter()
+router.register("tickets", TicketViewSet, basename="ticket")
+router.register("reservations", ReservationViewSet, basename="reservation")
+
+urlpatterns = [
+    path("", include(router.urls)),
+]
