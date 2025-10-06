790130dfbf1288edf81338c5042488a048e9413f
benfict <benfict@gmail.com>
2025-10-06 04:09:30 +0300

    add Docker file, .dockerignore and add postgres in settings

--- planetarium/settings.py
+++ planetarium/settings.py
@@ -128,9 +128,15 @@ WSGI_APPLICATION = "planetarium.wsgi.application"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {"ENGINE": "django.db.backends.sqlite3", "NAME": BASE_DIR / "db.sqlite3"}
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.environ["POSTGRES_DB"],
+        "USER": os.environ["POSTGRES_USER"],
+        "PASSWORD": os.environ["POSTGRES_PASSWORD"],
+        "HOST": os.environ["POSTGRES_HOST"],
+        "PORT": os.environ["POSTGRES_PORT"],
+    }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
